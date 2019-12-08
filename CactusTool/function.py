9ebef6014ec3938a50bee76ba734457b74936cb6
Yu Liu <yuliumutian@gmail.com>
2019-12-08 19:53:56 +0800

    update in 2019-12-08

---
+++ CactusTool/function.py
@@ -0,0 +1,11 @@
+def read(file):
+    """
+    Some file may be compression, can not open directly.
+
+    Args:
+        file (str): open file
+
+    Return:
+        The content of the file
+    """
+    return None
