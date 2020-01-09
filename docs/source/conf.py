7c7f5c9340b02ab094b9ea0a89662520acaac2ed
Yu Liu <yuliumutian@gmail.com>
2020-01-09 21:02:55 +0800

    update in 2020-01-09

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../CactusTool/'))
-
+autodoc_mock_imports = ['CactusTool']
 
 import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
